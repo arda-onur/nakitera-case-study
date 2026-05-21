399fcac1666d8cbffea326e1c8dcc53f3b2559af
Arda Onur <ardaonur15@gmail.com>
2026-05-21 11:32:39 +0300

    Generated main files via Spring Initializr

---
+++ src/main/java/com/ardao/nakitera_case_study/NakiteraCaseStudyApplication.java
@@ -0,0 +1,13 @@
+package com.ardao.nakitera_case_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NakiteraCaseStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NakiteraCaseStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ardao/nakitera_case_study/NakiteraCaseStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ardao.nakitera_case_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NakiteraCaseStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
