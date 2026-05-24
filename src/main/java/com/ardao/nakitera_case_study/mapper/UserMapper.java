9cd6459f4d2c5a55927f49d39a9e447deb016f2c
Arda Onur <ardaonur15@gmail.com>
2026-05-24 21:41:37 +0300

    feat(UserMapper): implement user mapper

---
+++ src/main/java/com/ardao/nakitera_case_study/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.ardao.nakitera_case_study.mapper;
+
+import com.ardao.nakitera_case_study.entity.User;
+import com.ardao.nakitera_case_study.request.customer.UserRequest;
+
+public class UserMapper {
+    public static User toEntity(UserRequest userRequest){
+        User mappedUser = new User();
+        mappedUser.setUsername(userRequest.username());
+        mappedUser.setPassword(userRequest.password());
+        return mappedUser;
+    }
+
+    private UserMapper(){
+
+    }
+}
