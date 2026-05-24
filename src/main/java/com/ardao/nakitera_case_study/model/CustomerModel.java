2dab33082363c35dc5db5fd6d9986687cbb19601
Arda Onur <ardaonur15@gmail.com>
2026-05-24 22:00:51 +0300

    feat(CustomerModel): implement  CustomerModel class

---
+++ src/main/java/com/ardao/nakitera_case_study/model/CustomerModel.java
@@ -0,0 +1,12 @@
+package com.ardao.nakitera_case_study.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CustomerModel {
+    private Long id;
+}
