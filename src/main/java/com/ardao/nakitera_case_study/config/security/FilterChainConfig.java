bf2b2e27716d451af40f6b04fc227460562b22f6
Arda Onur <ardaonur15@gmail.com>
2026-05-24 22:15:48 +0300

    feat(FilterChainConfig): implement FilterChain config class

---
+++ src/main/java/com/ardao/nakitera_case_study/config/security/FilterChainConfig.java
@@ -0,0 +1,70 @@
+package com.ardao.nakitera_case_study.config.security;
+
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.boot.security.autoconfigure.web.servlet.PathRequest;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class FilterChainConfig {
+    private final MessageSource messageSource;
+
+    public FilterChainConfig(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(PathRequest.toH2Console()).permitAll()
+                        .requestMatchers("/login","/auth/**").permitAll()
+                        .requestMatchers("/customer/create").hasRole("ADMIN")
+                        .anyRequest().authenticated())
+                .httpBasic(withDefaults())
+                .cors(withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+
+                .exceptionHandling(exception -> exception
+                        .authenticationEntryPoint((request, response, authException) -> {
+                            String message = messageSource.getMessage(
+                                    "authentication.required.exception",
+                                    null,
+                                    "Authentication is required.",
+                                    request.getLocale()
+                            );
+
+                            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                            response.setContentType("text/plain;charset=UTF-8");
+                            response.getWriter().write(message);
+                        })
+                        .accessDeniedHandler((request, response, accessDeniedException) -> {
+                            String message = messageSource.getMessage(
+                                    "access.denied.exception",
+                                    null,
+                                    "You do not have permission to access this resource.",
+                                    request.getLocale()
+                            );
+
+                            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+                            response.setContentType("text/plain;charset=UTF-8");
+                            response.getWriter().write(message);
+                        }))
+
+                .headers(headers -> headers
+                        .frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
+
+                .sessionManagement(
+                        session -> session
+                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+    }
+}
