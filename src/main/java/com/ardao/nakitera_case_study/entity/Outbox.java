3652cd3367d783a40a22ded3cd074188c9115d8e
Arda Onur <ardaonur15@gmail.com>
2026-05-24 21:35:57 +0300

    feat(Outbox): implement Outbox entity

---
+++ src/main/java/com/ardao/nakitera_case_study/entity/Outbox.java
@@ -0,0 +1,43 @@
+package com.ardao.nakitera_case_study.entity;
+
+import com.ardao.nakitera_case_study.enums.Side;
+import com.ardao.nakitera_case_study.enums.Status;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Outbox {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    @Column(nullable = false)
+    private Long customerId;
+
+    @Column(nullable = false)
+    private String assetName;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Side orderSide;
+
+    @Column(nullable = false)
+    private int size;
+
+    @Column(nullable = false)
+    private int price;
+
+    @Column(nullable = false)
+    private String eventType;
+
+    @Column(nullable = false)
+    private boolean published = false;
+}
