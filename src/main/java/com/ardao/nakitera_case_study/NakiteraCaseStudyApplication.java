3db757da2e2d462c59ba40d71019c9a1ba5c94f0
Arda Onur <ardaonur15@gmail.com>
2026-05-24 22:07:03 +0300

    feat(NakiteraCaseStudyApplication ): implement @EnableScheduling and @EnableWebSecurity annotation

--- src/main/java/com/ardao/nakitera_case_study/NakiteraCaseStudyApplication.java
+++ src/main/java/com/ardao/nakitera_case_study/NakiteraCaseStudyApplication.java
@@ -2,8 +2,12 @@ package com.ardao.nakitera_case_study;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableScheduling
+@EnableWebSecurity
 public class NakiteraCaseStudyApplication {
 
 	public static void main(String[] args) {
