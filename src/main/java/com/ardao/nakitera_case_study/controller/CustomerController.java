cc0132cbb68391f84fd0f7fd6c2bf2547ba9f10e
Arda Onur <ardaonur15@gmail.com>
2026-05-24 21:51:33 +0300

    feat(CustomerController): implement CustomerController class

---
+++ src/main/java/com/ardao/nakitera_case_study/controller/CustomerController.java
@@ -0,0 +1,38 @@
+package com.ardao.nakitera_case_study.controller;
+
+import com.ardao.nakitera_case_study.entity.Customer;
+import com.ardao.nakitera_case_study.repository.CustomerRepository;
+import com.ardao.nakitera_case_study.response.CustomerResponse;
+import com.ardao.nakitera_case_study.response.UserResponse;
+import com.ardao.nakitera_case_study.service.CustomerService;
+import org.springframework.context.MessageSource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
+
+@RestController
+@RequestMapping("/customer")
+public class CustomerController {
+         private final CustomerService customerService;
+         private final MessageSource messageSource;
+
+    public CustomerController(CustomerService customerService, MessageSource messageSource) {
+        this.customerService = customerService;
+        this.messageSource = messageSource;
+    }
+
+
+     @PostMapping("/create")
+    public ResponseEntity<CustomerResponse> createCustomer(Locale locale){
+        Customer customer = this.customerService.createCustomer();
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(
+                new CustomerResponse(customer.getId(),
+                        this.messageSource.getMessage("customer.response.created",
+                                null,  locale)));
+    }
+}
