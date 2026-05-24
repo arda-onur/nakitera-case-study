eeb57d99027b8fb7a145da34f0503e1f8a47d786
Arda Onur <ardaonur15@gmail.com>
2026-05-24 21:53:23 +0300

    feat(UserDetailsServiceImpl): implement UserDetailsServiceImpl class

---
+++ src/main/java/com/ardao/nakitera_case_study/service/userdetails/UserDetailsServiceImpl.java
@@ -0,0 +1,28 @@
+package com.ardao.nakitera_case_study.service.userdetails;
+
+import com.ardao.nakitera_case_study.entity.User;
+import com.ardao.nakitera_case_study.exception.custom.UserNotFoundException;
+import com.ardao.nakitera_case_study.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        log.info("Login attempt for username = {}", username);
+        User user = this.userRepository.findUserByUsername(username)
+                .orElseThrow(() -> new UserNotFoundException("user.not.found.exception", username));
+        return user;
+    }
+}
