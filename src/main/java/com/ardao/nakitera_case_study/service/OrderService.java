636fc10ebc321362efc15a2c220a0071fdd18f35
Arda Onur <ardaonur15@gmail.com>
2026-05-29 18:05:11 +0300

    feat(OrderService):Added cancelOrder,rejectOrder, matchOrders and  getCustomerOrders methods

--- src/main/java/com/ardao/nakitera_case_study/service/OrderService.java
+++ src/main/java/com/ardao/nakitera_case_study/service/OrderService.java
@@ -1,8 +1,15 @@
 package com.ardao.nakitera_case_study.service;
 
 import com.ardao.nakitera_case_study.entity.Order;
-import com.ardao.nakitera_case_study.model.CustomerModel;
+import com.ardao.nakitera_case_study.response.OrderListResponse;
+import org.springframework.data.domain.Page;
+
+import java.time.LocalDate;
 
 public interface OrderService {
-   void createOrder(Order order, CustomerModel customerModel);
+   void createOrder(Order order, Long customerId);
+   void cancelOrder(Long orderId);
+   void rejectOrder(Long orderId);
+   void matchOrders();
+   Page<OrderListResponse> getCustomerOrders(int page, int size, Long customerId, LocalDate startDate, LocalDate endDate);
 }
