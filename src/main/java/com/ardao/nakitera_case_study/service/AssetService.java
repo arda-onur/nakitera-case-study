688074fc9b094a84b59ed09f66842ba7d1e5eef4
Arda Onur <ardaonur15@gmail.com>
2026-05-29 17:37:48 +0300

    feat(AssetService): Implemented abstact methods getCustomerAssetList,handleMatchedOrderEvent,releaseReservedSellOrder

--- src/main/java/com/ardao/nakitera_case_study/service/AssetService.java
+++ src/main/java/com/ardao/nakitera_case_study/service/AssetService.java
@@ -2,10 +2,17 @@ package com.ardao.nakitera_case_study.service;
 
 import com.ardao.nakitera_case_study.entity.Asset;
 import com.ardao.nakitera_case_study.entity.Customer;
-import com.ardao.nakitera_case_study.entity.Outbox;
+import com.ardao.nakitera_case_study.event.dto.MatchedOrderEvent;
+import com.ardao.nakitera_case_study.event.dto.OrderEvent;
+import com.ardao.nakitera_case_study.response.AssetListResponse;
+import org.springframework.data.domain.Page;
 
 public interface AssetService {
     Asset createTRYAsset(Customer customer);
-    void handleOrderCreatedBuyEvent(Outbox outboxEvent);
-    void handleOrderCreatedSellEvent(Outbox outboxEvent);
+    void handleOrderCreatedBuyEvent(OrderEvent orderEvent);
+    void handleOrderCreatedSellEvent(OrderEvent orderEvent);
+    void releaseReservedBuyOrder(OrderEvent orderEvent);
+    void releaseReservedSellOrder(OrderEvent orderEvent);
+    void handleMatchedOrderEvent(MatchedOrderEvent matchedOrderEvent);
+    Page<AssetListResponse> getCustomerAssetList(int page, int size, Long customerId);
 }
