5e3d78c71168fb870b52a920d09624a6c25f1a79
Arda Onur <ardaonur15@gmail.com>
2026-05-29 18:00:25 +0300

    feat(OrderRepository): Implemented findById with pessimistic write lock and findAllByOrderStatus getOrdersByCustomer_IdAndCreateDateBetween

---
+++ src/main/java/com/ardao/nakitera_case_study/repository/OrderRepository.java
@@ -0,0 +1,24 @@
+package com.ardao.nakitera_case_study.repository;
+
+import com.ardao.nakitera_case_study.entity.Order;
+import com.ardao.nakitera_case_study.enums.Status;
+import jakarta.persistence.LockModeType;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.Optional;
+
+public interface OrderRepository extends JpaRepository<Order,Long> {
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    Optional<Order> findById(Long id);
+    List<Order> findAllByOrderStatus(Status orderStatus);
+    Page<Order> getOrdersByCustomer_IdAndCreateDateBetween(long customerId,
+                                                           Instant createDateAfter,
+                                                           Instant createDateBefore,
+                                                           Pageable pageable);
+
+}
