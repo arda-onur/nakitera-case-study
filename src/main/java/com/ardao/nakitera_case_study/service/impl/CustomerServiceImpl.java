001d1566abf79b464874a19bd3107a0957fd3ede
Arda Onur <ardaonur15@gmail.com>
2026-05-29 17:42:28 +0300

    feat(CustomerServiceImpl):  Added logger for createCustomer method

--- src/main/java/com/ardao/nakitera_case_study/service/impl/CustomerServiceImpl.java
+++ src/main/java/com/ardao/nakitera_case_study/service/impl/CustomerServiceImpl.java
@@ -4,9 +4,12 @@ import com.ardao.nakitera_case_study.entity.Customer;
 import com.ardao.nakitera_case_study.repository.CustomerRepository;
 import com.ardao.nakitera_case_study.service.AssetService;
 import com.ardao.nakitera_case_study.service.CustomerService;
+import jakarta.transaction.Transactional;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class CustomerServiceImpl implements CustomerService {
     private final AssetService assetService;
     private final CustomerRepository customerRepository;
@@ -17,10 +20,13 @@ public class CustomerServiceImpl implements CustomerService {
     }
 
     @Override
+    @Transactional
     public Customer createCustomer() {
+        log.info("Creating customer.");
         Customer newCustomer = new Customer();
         newCustomer.getAssetList().add(this.assetService.createTRYAsset(newCustomer));
         this.customerRepository.save(newCustomer);
+        log.info("Customer created successfully. customerId={}", newCustomer.getId());
         return newCustomer;
     }
 }
