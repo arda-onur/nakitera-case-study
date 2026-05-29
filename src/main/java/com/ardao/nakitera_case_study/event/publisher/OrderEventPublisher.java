82867bff5de7b86170f80a24e0ffa5eb728445dd
Arda Onur <ardaonur15@gmail.com>
2026-05-29 17:55:06 +0300

    feat(OrderEventPublisher): Implemented OrderEvent Publisher class

--- src/main/java/com/ardao/nakitera_case_study/event/publisher/OrderEventPublisher.java
+++ src/main/java/com/ardao/nakitera_case_study/event/publisher/OrderEventPublisher.java
@@ -1,8 +1,13 @@
 package com.ardao.nakitera_case_study.event.publisher;
 
-import com.ardao.nakitera_case_study.entity.Outbox;
-import com.ardao.nakitera_case_study.repository.OutboxRepository;
+import com.ardao.nakitera_case_study.entity.box.MatchedOrderOutbox;
+import com.ardao.nakitera_case_study.entity.box.OrderOutbox;
+import com.ardao.nakitera_case_study.event.dto.MatchedOrderEvent;
+import com.ardao.nakitera_case_study.event.dto.OrderEvent;
+import com.ardao.nakitera_case_study.repository.box.MatchedOrderOutboxRepository;
+import com.ardao.nakitera_case_study.repository.box.OrderOutboxRepository;
 import jakarta.transaction.Transactional;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -10,24 +15,47 @@ import org.springframework.stereotype.Component;
 import java.util.List;
 
 @Component
+@Slf4j
 public class OrderEventPublisher {
 
-    private final OutboxRepository outboxRepository;
-    private final KafkaTemplate<String, Outbox> kafkaTemplate;
+    private final OrderOutboxRepository orderOutboxRepository;
+    private final KafkaTemplate<String, OrderEvent> kafkaTemplate;
 
-    public OrderEventPublisher(OutboxRepository outboxRepository, KafkaTemplate<String, Outbox> kafkaTemplate) {
-        this.outboxRepository = outboxRepository;
+
+
+    public OrderEventPublisher(OrderOutboxRepository orderOutboxRepository, KafkaTemplate<String, OrderEvent> kafkaTemplate) {
+        this.orderOutboxRepository = orderOutboxRepository;
         this.kafkaTemplate = kafkaTemplate;
     }
     @Scheduled(fixedDelay = 5000)
     @Transactional
-    public void publishEvents(){
-         List<Outbox> unpublishedEvents = this.outboxRepository.findByPublishedFalse();
+    public void publishOrderEvents(){
+        List<OrderOutbox> unpublishedEvents = this.orderOutboxRepository.findByPublishedAndEventTypeInOrderByIdAsc(
+                false,
+                List.of("ORDER_CREATED", "ORDER_CANCELED"));
 
-         for(Outbox unpublishedEvent : unpublishedEvents){
-             kafkaTemplate.send("ORDER_EVENT", unpublishedEvent);
-             unpublishedEvent.setPublished(true);
-         }
+        if(!unpublishedEvents.isEmpty()){
+            for(OrderOutbox unpublishedEvent : unpublishedEvents){
+                OrderEvent orderEvent = new OrderEvent(
+                        unpublishedEvent.getId(),
+                        unpublishedEvent.getOrderId(),
+                        unpublishedEvent.getCustomerId(),
+                        unpublishedEvent.getAssetName(),
+                        unpublishedEvent.getOrderSide(),
+                        unpublishedEvent.getSize(),
+                        unpublishedEvent.getPrice(),
+                        unpublishedEvent.getEventType());
+
+                try {
+                    log.info("Publishing ORDER_EVENT.outboxId={}",unpublishedEvent.getId());
+                    kafkaTemplate.send("ORDER_EVENT", orderEvent).get();
+                    unpublishedEvent.setPublished(true);
+                    log.info("ORDER_EVENT published successfully. outboxId={}",
+                            unpublishedEvent.getId());
+                }catch (Exception e){
+                    log.error("Could not send ORDER_EVENT outboxId={}",unpublishedEvent.getId());
+                }
+            }
+        }
     }
-
 }
