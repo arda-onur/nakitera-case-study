ba211203a9dedaed9a22c978e8f4701b34621c83
Arda Onur <ardaonur15@gmail.com>
2026-05-30 13:06:33 +0300

    feat(RedisRateLimiterFilter) : Implemented RedisRateLimiterFilter class

---
+++ src/main/java/com/ardao/nakitera_case_study/config/security/filter/RedisRateLimiterFilter.java
@@ -0,0 +1,38 @@
+package com.ardao.nakitera_case_study.config.security.filter;
+
+import com.ardao.nakitera_case_study.service.redis.RedisRateLimiterService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class RedisRateLimiterFilter extends OncePerRequestFilter {
+
+    private final RedisRateLimiterService redisRateLimiterService;
+
+    public RedisRateLimiterFilter(RedisRateLimiterService redisRateLimiterService) {
+        this.redisRateLimiterService = redisRateLimiterService;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        String key = "rate-limit" + request.getRemoteAddr();
+
+
+        boolean isAllowed = this.redisRateLimiterService.isAllowed(key);
+
+        if(!isAllowed){
+            response.setStatus(429);
+            response.setContentType("text/plain;charset=UTF-8");
+            response.getWriter().write("Too many request please wait!");
+            return;
+        }
+        filterChain.doFilter(request,response);
+    }
+}
